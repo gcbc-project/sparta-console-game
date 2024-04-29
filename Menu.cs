020bfcbaf6d806006eebe9ff1f71cd221efb0957
l11040 <113540683+l11040@users.noreply.github.com>
2024-04-29 19:04:07 +0900

    Update : 게임 플로우 작성

--- Menu.cs
+++ Menu.cs
@@ -9,18 +9,19 @@ namespace SpartaConsoleGame
 {
     public struct MenuItem
     {
-        public string Label {  get; private set; }
-        public Action Action {  get; private set; }
+        public string Label { get; private set; }
+        public Action Action { get; private set; }
         public MenuItem(string label, Action action)
-        {  
-            Label = label; Action = action; 
+        {
+            Label = label; Action = action;
         }
     }
-    
+
     internal class Menu
     {
         public string Title { get; private set; }
         public string Description { get; private set; }
+        public string Info { get; private set; }
         private List<MenuItem> menuItems;
 
 
@@ -39,52 +40,31 @@ namespace SpartaConsoleGame
             Description = desc;
         }
 
+        public void SetInfo(string info)
+        {
+            Info = info;
+        }
+
         public void AddMenuItem(string option, Action action)
         {
-            
+
             menuItems.Add(new MenuItem(option, action));
         }
-        
+
         public void Display()
         {
             Console.WriteLine(Title);
             Console.WriteLine(Description);
+            Console.WriteLine(Info);
             for (int i = 0; i < menuItems.Count; i++)
             {
-                Console.WriteLine($"{i+1}. {menuItems[i].Label}");
+                Console.WriteLine($"{i + 1}. {menuItems[i].Label}");
             }
         }
 
 
         // 메인 화면
-        public void MainMenu()
-        {
-            Console.Clear();
 
-            Menu mainMenu = new Menu();
-
-            mainMenu.SetDesc("스파르타 마을에 오신 것을 환영합니다! \n이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.");
-
-            mainMenu.AddMenuItem("상태보기", SatusMenu);
-            mainMenu.AddMenuItem("인벤토리", () => Console.WriteLine("인벤토리 소환"));
-            mainMenu.AddMenuItem("상점", () => Console.WriteLine("상점 소환"));
-
-            mainMenu.Run();
-        }
-
-        //스테이터스 콜백
-        public void SatusMenu()
-        {
-            Console.Clear();
-
-            Menu mainMenu = new Menu();
-            
-            mainMenu.SetTitle("[스테이터스]");
-
-            //player.GetStatus();
-            
-            mainMenu.Run();
-        }
         //인벤토리 콜백
 
         //샵 콜백
@@ -112,17 +92,17 @@ namespace SpartaConsoleGame
 
         public void Run()
         {
-            while(true)
+            while (true)
             {
                 Console.Clear();
 
                 Display();
                 int choice = HandleChoice();
-                if ( choice > 0 )
+                if (choice > 0)
                 {
                     menuItems[choice - 1].Action.Invoke();
                 }
-                else if ( choice == 0 )
+                else if (choice == 0)
                 {
                     break;
                 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,35 @@
-﻿internal class Program
+﻿using SpartaConsoleGame;
+
+internal class Program
 {
+    static Player player = new Player("르탄", "전사");
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.ReadLine();
+        Menu mainMenu = new Menu();
+
+        mainMenu.SetDesc("스파르타 마을에 오신 것을 환영합니다! \n이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.");
+
+        mainMenu.AddMenuItem("상태보기", () =>
+        {
+            Console.WriteLine("test");
+            Console.ReadKey();
+            SatusMenu();
+        });
+        mainMenu.AddMenuItem("인벤토리", () => Console.WriteLine("인벤토리 소환"));
+        mainMenu.AddMenuItem("상점", () => Console.WriteLine("상점 소환"));
+
+        mainMenu.Run();
+    }
+
+    public static void SatusMenu()
+    {
+        Console.Clear();
+
+        Menu mainMenu = new Menu();
+
+        mainMenu.SetTitle("[스테이터스]");
+        mainMenu.SetInfo(player.GetStatus());
+
+        mainMenu.Run();
     }
 }
