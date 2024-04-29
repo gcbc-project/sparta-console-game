87a4ade052218e826bf2be53f79fc080cab2a34b
JeongSeok <97824309+RryNoel@users.noreply.github.com>
2024-04-29 17:13:30 +0900

    Add : ShopItem
    Add Shop Items

---
+++ Shop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaConsoleGame
+{
+    internal class Shop
+    {
+        public List<ShopItem> Items { get; set; }
+
+
+        public Shop()
+        {
+            Items = new List<ShopItem>();
+        }
+
+        public void Buy(Player player, int index)
+        {
+            if (Items[index].Price <= player.Gold)
+            {
+                Items[index].IsPurchased = true;
+                // player.Inventory.Items.Add((InventoryItem)Items[index]);
+            }
+        }
+    }
+}
