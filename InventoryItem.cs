cfb9aae346ae8358b8afd499c9cb1c8a326d7553
JeongSeok <97824309+RryNoel@users.noreply.github.com>
2024-04-29 12:50:18 +0900

    Add : InventoryItem
    Add InventoryItem

---
+++ InventoryItem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaConsoleGame
+{
+    internal class InventoryItem : Item
+    {
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public int Atk { get; set; }
+        public int Def { get; set; }
+        public int Price { get; set; }
+        public bool IsEquiped { get; set; }
+
+        public InventoryItem(string name, string desc, int price, int atk = 0, int def = 0)
+        {
+            Name = name;
+            Desc = desc;
+            Price = price;
+            Atk = atk;
+            Def = def;
+        }
+
+        public string GetItemInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            if (IsEquiped)
+            {
+                sb.Append("[E]");
+            }
+            sb.Append($"{Name}\t|");
+            if(Atk != 0)
+            {
+                sb.Append($"공격력 {Atk}\t|");
+            }
+            if (Def != 0)
+            {
+                sb.Append($"방어력 {Def}\t|");
+            }
+            sb.Append($"{Desc}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaConsoleGame
+{
+    internal interface Item
+    {
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public int Atk { get; set; }
+        public int Def { get; set; }
+        public int Price { get; set; }
+
+        public string GetItemInfo();
+    }
+}
