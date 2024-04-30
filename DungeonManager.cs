cca17de22ab9c95c15288fa2b99b07aef94988e7
JeongSeok <97824309+RryNoel@users.noreply.github.com>
2024-04-30 16:23:19 +0900

    Add : 던전 기능
    Add Dungeon

---
+++ DungeonManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaConsoleGame
+{
+    internal class DungeonManager
+    {
+        public List<Dungeon> DungeonList { get; set; }
+
+        public Random random = new Random();
+
+        public DungeonManager()
+        { 
+            DungeonList = new List<Dungeon>();
+        }
+
+        public string Enter(Player player,int index)
+        {
+            StringBuilder sb = new StringBuilder();
+            // Hp와 관련된 정의
+            
+            if(player.CalculateDef < DungeonList[index].RecommendDef && random.Next(0, 10) < 4)
+            {
+                sb.AppendLine($"{DungeonList[index].Title}을 클리어하지 못했습니다.");
+                sb.AppendLine("\n[탐험 결과]");
+                sb.AppendLine($"체력 {player.Hp} -> {player.Hp / 2}");
+                player.Hp /= 2;
+            }
+            else
+            {
+                int defValve = DungeonList[index].RecommendDef - player.CalculateDef;
+                int baseHpLoss = random.Next(20 + defValve, 36 + defValve);
+                int extraRewardPer = random.Next(player.CalculateAtk, player.CalculateAtk * 2);
+                int extraReward = (int)(DungeonList[index].BasicReward * (extraRewardPer / 100.0f));
+                int totalReward = DungeonList[index].BasicReward + extraReward;
+                sb.AppendLine($"{DungeonList[index].Title}을 클리어하였습니다.");
+                sb.AppendLine("\n[탐험 결과]");
+                sb.AppendLine($"체력 {player.Hp} -> {player.Hp - baseHpLoss}");
+                sb.AppendLine($"Gold {player.Gold} -> {player.Gold + totalReward}");
+                player.Hp -= baseHpLoss;
+                player.Gold += totalReward;
+            }
+            return sb.ToString();
+        }
+    }
+}
