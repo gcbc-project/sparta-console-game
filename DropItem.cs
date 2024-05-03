3e74c0ccd70dc2982f96d31179c5c0e4fec05045
l11040 <113540683+l11040@users.noreply.github.com>
2024-05-03 11:15:18 +0900

    Add : DropItem 클래스 추가

---
+++ DropItem.cs
@@ -0,0 +1,28 @@
+﻿using SpartaConsoleGame.Enemy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaConsoleGame
+{
+    internal class DropItem : IItem
+    {
+        public Item BaseItem { get; set; }
+        public float DropRate { get; set; }
+
+        public DropItem(Item item)
+        {
+            BaseItem = item;
+        }
+
+        public string GetItemInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            // TODO : 리워드시 표시될 Info 작성
+
+            return sb.ToString();
+        }
+    }
+}
